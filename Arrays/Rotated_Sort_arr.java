d3d67e4dd9ad4a2b76c9f7808333f4dfe0a40337
Sanika Ghugare <144719999+GhugareSanika@users.noreply.github.com>
2024-07-17 14:04:20 +0530

    Arrrays

---
+++ Arrays/Rotated_Sort_arr.java
@@ -0,0 +1,33 @@
+package Arrays;
+/*-------------------Search in Rotated Sorted Array---------------------
+    Input: nums = [4,5,6,7,0,1,2], target = 0
+    Output: 4
+ */
+
+ 
+public class Rotated_Sort_arr {
+    public int search(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+           int mid=(low+high)/2;
+           if(nums[mid]==target){
+               return mid;
+           }
+           if(nums[low]<=nums[mid]){
+               if(target>=nums[low] && target<nums[mid]){
+                   high=mid-1;
+               }else{
+                   low=mid+1;
+               }
+           }else{
+               if(target>nums[mid] && target <=nums[high]){
+                   low=mid+1;
+               }else{
+                   high=mid-1;
+               }
+           }
+       }
+       return -1;
+   }
+}
