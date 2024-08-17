557b263d803ab29d4c0d2fc287e57daa846645ec
Sanika Ghugare <144719999+GhugareSanika@users.noreply.github.com>
2024-08-17 08:28:46 +0530

    spiral_matrix

--- 2D_Arrays/spiral_matrix.java
+++ 2D_Arrays/spiral_matrix.java
@@ -1,4 +1,4 @@
-/*--------------------------------Spiral Matrix---------------------------
+/*--------------------------------Spiral Matrix-------------------------------
  
     Given an m x n matrix, return all elements of the matrix in spiral order.
     Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
